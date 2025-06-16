0be34a4c73cc820974b93a7c05b1fac5343e7225
DevanoAlif22 <devanorama123@gmail.com>
2025-06-16 22:06:00 +0700

    fix : router

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 // app/page.tsx
-import HeroSection from "@/app/(main)/components/hero-section";
-import AboutSection from "@/app/(main)/components/about-section";
-import ExperienceSection from "@/app/(main)/components/experience-section";
-import ServicesSection from "@/app/(main)/components/services-section";
-import SkillsSection from "@/app/(main)/components/skills-section";
-import WorkSection from "@/app/(main)/components/work-section";
-import MarqueeSection from "@/app/(main)/components/marquee-section";
+import HeroSection from "@/app/main/components/hero-section";
+import AboutSection from "@/app/main/components/about-section";
+import ExperienceSection from "@/app/main/components/experience-section";
+import ServicesSection from "@/app/main/components/services-section";
+import SkillsSection from "@/app/main/components/skills-section";
+import WorkSection from "@/app/main/components/work-section";
+import MarqueeSection from "@/app/main/components/marquee-section";
 
 export default function Home() {
   return (
